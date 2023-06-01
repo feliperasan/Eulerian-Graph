56c70c9abe22410690f5bb313dac39f8f3dfa963
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2023-06-01 13:12:45 -0300

    Grafo Ponderado

--- grafo.c
+++ grafo.c
@@ -3,6 +3,7 @@
 
 typedef struct No {
   int dest;
+  int peso;
   struct No* prox;
 } No;
 
@@ -12,9 +13,10 @@ typedef struct Grafo {
 } Grafo;
 
 // Cria um Nó
-No* criarNo(int dest) {
+No* criarNo(int dest, int peso) {
   No* novoNo = (No*)malloc(sizeof(No));
   novoNo -> dest = dest;
+  novoNo -> peso = peso;
   novoNo -> prox = NULL;
   return novoNo;
 }
@@ -36,14 +38,14 @@ Grafo* criarGrafo(int vertice) {
 }
 
 // Adiciona Aresta não direcionada ao Grafo
-void adicionarAresta(Grafo* grafo, int origem, int dest) {
+void adicionarAresta(Grafo* grafo, int origem, int dest, int peso) {
   // Adiciona uma aresta do vertice de origem para o destino
-  No* novoNo = criarNo(dest);
+  No* novoNo = criarNo(dest, peso);
   novoNo -> prox = grafo -> adjList[origem];
   grafo -> adjList[origem] = novoNo;
 
   // Adiciona uma aresta do vertice de destino para a origem
-  novoNo = criarNo(origem);
+  novoNo = criarNo(origem, peso);
   novoNo -> prox = grafo -> adjList[dest];
   grafo -> adjList[dest] = novoNo;
 }
@@ -54,7 +56,7 @@ void printGrafo(Grafo* grafo) {
     No* temp = grafo -> adjList[i];
     printf("Lista de adjacencia do vertice %d\n", i);
     while (temp) {
-      printf(" -> %d", temp -> dest);
+      printf(" -> %d [Peso: %d]", temp -> dest, temp -> peso);
       temp = temp -> prox;
     }
 
@@ -67,13 +69,14 @@ int main() {
   int v = 5;
   Grafo* grafo = criarGrafo(v);
 
-  adicionarAresta(grafo, 0, 1);
-  adicionarAresta(grafo, 0, 4);
-  adicionarAresta(grafo, 1, 2);
-  adicionarAresta(grafo, 1, 3);
-  adicionarAresta(grafo, 1, 4);
-  adicionarAresta(grafo, 2, 3);
-  adicionarAresta(grafo, 3, 4);
+  // Grafo, Origim, destino, peso da aresta
+  adicionarAresta(grafo, 0, 1, 2);
+  adicionarAresta(grafo, 0, 4, 5);
+  adicionarAresta(grafo, 1, 2, 3);
+  adicionarAresta(grafo, 1, 3, 1);
+  adicionarAresta(grafo, 1, 4, 4);
+  adicionarAresta(grafo, 2, 3, 7);
+  adicionarAresta(grafo, 3, 4, 6);
 
   printGrafo(grafo);
 
